4d761d1671e6a9f6fb457ee329c7abfa3d4148fe
stepa <gershuni.stepan@gmail.com>
2024-07-27 14:25:59 +0100

    streaming

--- src/app/agi/page.tsx
+++ src/app/agi/page.tsx
@@ -1,9 +1,28 @@
+'use client'
+
 import { Link } from "@telegram-apps/telegram-ui";
+import { openai } from '@ai-sdk/openai';
+import { generateText, streamText } from 'ai';
+import { useCompletion } from 'ai/react';
+
 
 export default function AGI() {
+  const { completion, complete } = useCompletion({
+    api: '/api/completion',
+  });
+  
   return (
     <div>
-      <div>AGI</div>
+      <div>
+        <div
+          onClick={async () => {
+            await complete('Why is the sky blue?');
+          }}
+        >
+          Generate
+        </div>
+        {completion}
+      </div>
       <Link href='/'>
         Back
       </Link>
